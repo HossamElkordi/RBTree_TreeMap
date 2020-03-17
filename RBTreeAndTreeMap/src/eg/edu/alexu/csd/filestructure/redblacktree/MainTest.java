8baa7b74d5658b2e91857e1797c6b76832f1760c
HossamElkordi <hossam_elkordi@yahoo.com>
2020-03-17 19:25:44 +0200

    MainTest Added

---
+++ RBTreeAndTreeMap/src/eg/edu/alexu/csd/filestructure/redblacktree/MainTest.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		IRedBlackTree<Integer, Integer> rbTree = new RBTree<Integer, Integer>();
+		rbTree.insert(5, 5);
+		rbTree.insert(4, 4);
+		rbTree.insert(3, 3);
+		rbTree.insert(2, 2);
+		rbTree.insert(1, 1);
+		inOrderTraversal(rbTree.getRoot());
+	}
+
+	private static void inOrderTraversal(INode<Integer, Integer> root) {
+		if(root.isNull()) return;
+		inOrderTraversal(root.getLeftChild());
+		System.out.println("(" + root.getKey() + ", " + root.getValue() + "): " + ((root.getColor() == INode.RED) ? "RED" : "BLACK"));
+		inOrderTraversal(root.getLeftChild());
+	}
+}
